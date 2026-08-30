1ddeeece25dd840524f2ef5ebdb125491678f0d4
Tushar <116452271+Gittushar88448@users.noreply.github.com>
2026-08-30 15:48:18 +0000

    remove console log statement

--- zoom_clone/src/components/proctedAuth/openRoute.jsx
+++ zoom_clone/src/components/proctedAuth/openRoute.jsx
@@ -2,8 +2,6 @@ import { Navigate } from "react-router-dom";
 
 const OpenRoute = ({children}) => {
     let token = JSON.parse(localStorage.getItem('token'));
-    console.log("open route token", token);
-    
     if(token == null || token == undefined){
         return children;
     }else{
